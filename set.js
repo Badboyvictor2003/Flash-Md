7e4d23805abdb715c48f4e9338ecc5aa79a09551
Badboyvictor2003 <prettyqueen22457@gmail.com>
2024-06-09 23:27:18 +0100

    Update set.js

--- set.js
+++ set.js
@@ -10,7 +10,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
 module.exports = { session: process.env.SESSION_ID || 'FLASH-MD-WA-BOT;;;=>',
     PREFIXE: process.env.PREFIX || "+",
     OWNER_NAME: process.env.OWNER_NAME || "badboy victor",
-    NUMERO_OWNER : process.env.OWNER_NUMBER || "2348121303497", 
+    NUMERO_OWNER : process.env.OWNER_NUMBER || "2349035745677", 
              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
 AUTOREAD_MESSAGES: process.env.AUTO_READ_MESSAGES || "no",
